0e3c69b1125fe940a194cf5ec553e41b65ee6d28
HaiderAli777 <ha7325897@gmail.com>
2024-11-29 09:47:41 -0800

    Adding Chatbot Functionality

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import FloatingChatbot from "./pages/FloatingChatbot"; // Import the chatbot component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      <FloatingChatbot />
     </BrowserRouter>
   </Provider>
 );
